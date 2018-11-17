384c772d2167e50755b66048f3315776a2e291e6
chevisutton <cheviksutton@gmail.com>
2018-11-16 22:21:41 -0700

    Route gaurd added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,17 +5,19 @@ import { PostCurrPostsComponent } from './posts/post-currPosts/post-currPosts.co
 import { PostAddComponent } from './posts/post-createBlog/post-add.component';
 import { LoginComponent } from './auth/login/login.component';
 import { SignUpComponent } from './auth/sign-up/sign-up.component';
+import { AuthGaurd } from './auth/auth.gaurd';
 
 const routes: Routes = [
   { path: '', component: PostCurrPostsComponent },
-  { path: 'newPost', component: PostAddComponent },
-  { path: 'edit/:postId', component: PostAddComponent }, // /:postId is Dynamic segment
+  { path: 'newPost', component: PostAddComponent, canActivate: [AuthGaurd] },
+  { path: 'edit/:postId', component: PostAddComponent, canActivate: [AuthGaurd] }, // /:postId is Dynamic segment
   { path: 'login', component: LoginComponent },
   { path: 'signUp', component: SignUpComponent }
 ];
 
 @NgModule({
   imports: [ RouterModule.forRoot(routes)],
-  exports: [ RouterModule ]
+  exports: [ RouterModule ],
+  providers: [AuthGaurd]
 })
 export class AppRoutingModule {}
