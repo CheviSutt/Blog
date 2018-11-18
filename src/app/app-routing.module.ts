bdad12ee388d3fb2246bffa7558e59199f08ee69
chevisutton <cheviksutton@gmail.com>
2018-11-17 18:41:22 -0700

    Lazy Loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,19 @@
 import { NgModule } from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
-
+import { RouterModule, Routes } from '@angular/router';
 import { PostCurrPostsComponent } from './posts/post-currPosts/post-currPosts.component';
 import { PostAddComponent } from './posts/post-createBlog/post-add.component';
-import { LoginComponent } from './auth/login/login.component';
-import { SignUpComponent } from './auth/sign-up/sign-up.component';
 import { AuthGaurd } from './auth/auth.gaurd';
 
 const routes: Routes = [
   { path: '', component: PostCurrPostsComponent },
   { path: 'newPost', component: PostAddComponent, canActivate: [AuthGaurd] },
   { path: 'edit/:postId', component: PostAddComponent, canActivate: [AuthGaurd] }, // /:postId is Dynamic segment
-  { path: 'login', component: LoginComponent },
-  { path: 'signUp', component: SignUpComponent }
+  { path: 'auth', loadChildren: './auth/auth.module#AuthModule'}
 ];
 
 @NgModule({
   imports: [ RouterModule.forRoot(routes)],
   exports: [ RouterModule ],
-  providers: [AuthGaurd]
+  providers: [ AuthGaurd ]
 })
 export class AppRoutingModule {}
